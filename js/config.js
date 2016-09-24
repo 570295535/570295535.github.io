fa628d6b7089a0ef0734617eeb61eb5d8c58fee3
570295535 <570295535@qq.com>
2016-09-25 03:23:31 +0800

    1

--- js/config.js
+++
@@ -1,5 +0,0 @@
-config = {
-    blog_name: "肖施主 ● 博客",
-    per_page: 10,             // 首页每次载入文章列表数量
-    file_server: "https://570295535.github.io:8001",
-};
--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-filepath=$(cd $(dirname $0) && pwd)
-cd $filepath
-python -m SimpleHTTPServer 80 >md-pages.log 2>&1 &
-python ../http-file-server/file-server.py ../http-file-server/files 8001 >file-server.log 2>&1 &
-
